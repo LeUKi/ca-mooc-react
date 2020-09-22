2858522ac0e337ace0a253a021f99bbc8d0c66dd
lafish <lafish@lafishdeiMac-Pro.local>
2020-09-22 19:58:10 +0800

    1

--- src/guest/guest.jsx
+++ src/guest/guest.jsx
@@ -6,15 +6,26 @@ import Communication from './communication'
 import Home from './home'
 import Query from './query'
 
-import {Button} from 'antd';
+
 import 'antd/dist/antd.css'
+
+
 
 function Guest({ match }) {
   return <div className="Guest" >
-<Button>123</Button>
+    <div className='banner'>
+      <div className='text'>
+        <span className='text1'>电路分析</span>
+        <span className='text2'>精品课程</span>
+      </div>
+    </div>
 
-    我是guest Bar
-<Router>
+<ul>
+  <li></li>
+</ul>
+
+
+    <Router>
       <Switch>
         <Route path={match.url + "/communication"} component={Communication}></Route>
         <Route path={match.url + "/home"} component={Home}></Route>
--- src/guest/home.jsx
+++ src/guest/home.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Carousel } from 'antd';
+import banner from './banner.jpg'
 
+
+const contentStyle = {
+    width: '600px',
+    height: '32vh',
+    color: '#fff',
+    lineHeight: '160px',
+    textAlign: 'center',
+    background: '#364d79',
+    backgroundSize: 'cover',
+  };
 
 function Admin() {
     return <div className="Admin" >
-        我是guest下的 home
-    </div>
+        <Carousel autoplay>
+            <div>
+                <img style={contentStyle} src={banner} alt='pic' />
+            </div>
+            <div>
+                <h3 style={contentStyle}>2</h3>
+            </div>
+        </Carousel>    </div>
 }
 
 export default Admin;
