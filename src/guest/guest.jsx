610ff30a02ac96a077d54dd61fe42cb53d199d74
lafish <lafish@lafishdeiMac-Pro.local>
2020-09-23 15:27:39 +0800

    z

--- src/guest/guest.jsx
+++ src/guest/guest.jsx
@@ -42,7 +42,7 @@ class Sider extends React.Component {
 
   render() {
     return (
-      <>
+      <div className={'noselect'}>
         <Switch
           checked={this.state.theme === 'dark'}
           onChange={this.changeTheme}
@@ -56,20 +56,20 @@ class Sider extends React.Component {
           selectedKeys={[this.state.current]}
           mode="horizontal"
         >
-          <Menu.Item key="1" icon={<HomeOutlined />}><Link to={'/guest/home'}>首页</Link></Menu.Item>
+          <Menu.Item key="1" icon={<HomeOutlined />}><Link to={'/guest/home'}>首 页</Link></Menu.Item>
           <Menu.Item key="2" icon={<PlaySquareOutlined />}><Link to={'/guest/wlk'}>网络课</Link></Menu.Item>
           <Menu.Item key="3" icon={<CheckSquareOutlined />}><Link to={'/guest/zxpc'}>在线测评</Link></Menu.Item>
-          <Menu.Item key="4" icon={<AudioOutlined />}><Link to={'/guest/jz'}>讲座</Link></Menu.Item>
+          <Menu.Item key="4" icon={<AudioOutlined />}><Link to={'/guest/jz'}>讲 座</Link></Menu.Item>
           <Menu.Item key="5" icon={<TeamOutlined />}><Link to={'/guest/sjjx'}>实践教学</Link></Menu.Item>
           <Menu.Item key="6" icon={<CommentOutlined />}><Link to={'/guest/hdjl'}>互动交流</Link></Menu.Item>
           <Button
             size='small'
             ghost
-            style={{ 'float': 'right',margin:'10px' }}
+            style={{ 'float': 'right', margin: '10px' }}
           ><Link to={'/login'}>后台登入</Link></Button>
 
         </Menu>
-      </>
+      </div>
     );
   }
 }
@@ -79,7 +79,7 @@ class Sider extends React.Component {
 class Guest extends React.Component {
   render() {
     return <div className="Guest" >
-      <div className='banner'>
+      <div className='banner noselect'>
         <div className='text'>
           <span className='text1'>电路分析</span>
           <span className='text2'>精品课程</span>
@@ -88,19 +88,22 @@ class Guest extends React.Component {
 
       <Sider />
 
-      <Router history={history}>
-        <Switch>
-          <Route path={"/guest/hdjl"} component={Communication}></Route>
-          <Route path={"/guest/home"} component={Home}></Route>
-          <Route path={"/guest/wlk"} component={Query}></Route>
-          <Route path={"/guest/zxpc"} component={Query}></Route>
-          <Route path={"/guest/jz"} component={Query}></Route>
-          <Route path={"/guest/sjjx"} component={Query}></Route>
-          <Route path={'/guest'}>
-            <Redirect to={'/guest/home'}></Redirect>
-          </Route>
-        </Switch>
-      </Router>
+      <div>
+
+        <Router history={history}>
+          <Switch>
+            <Route path={"/guest/hdjl"} component={Communication}></Route>
+            <Route path={"/guest/home"} component={Home}></Route>
+            <Route path={"/guest/wlk"} component={Query}></Route>
+            <Route path={"/guest/zxpc"} component={Query}></Route>
+            <Route path={"/guest/jz"} component={Query}></Route>
+            <Route path={"/guest/sjjx"} component={Query}></Route>
+            <Route path={'/guest'}>
+              <Redirect to={'/guest/home'}></Redirect>
+            </Route>
+          </Switch>
+        </Router>
+      </div>
     </div>
   }
 }
--- src/guest/home.jsx
+++ src/guest/home.jsx
@@ -1,28 +1,16 @@
 import React from 'react';
-import { Carousel } from 'antd';
-import banner from './banner.jpg'
+import { Row, Col } from 'antd';
 
-
-const contentStyle = {
-    width: '600px',
-    height: '32vh',
-    color: '#fff',
-    lineHeight: '160px',
-    textAlign: 'center',
-    background: '#364d79',
-    backgroundSize: 'cover',
-  };
 
 function Home() {
     return <div className="Home" key='home'>
-        <Carousel autoplay>
-            <div>
-                <img style={contentStyle} src={banner} alt='pic' />
-            </div>
-            <div>
-                <h3 style={contentStyle}>2</h3>
-            </div>
-        </Carousel>    </div>
+
+        <Row >
+            <Col span={2} className={'test'}>123</Col>
+            <Col span={2} className={'test'}>123</Col>
+            <Col span={2} className={'test'}>123</Col>
+        </Row>
+    </div>
 }
 
 export default Home;
