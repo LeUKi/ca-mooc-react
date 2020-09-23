81415e23761ad71dee1f685d5a0924f896a0323e
lafish <lafish@lafishdeiMac-Pro.local>
2020-09-23 17:46:13 +0800

    1

--- src/admin/admin.jsx
+++ src/admin/admin.jsx
@@ -1,21 +1,76 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 import './admin.scss'
+import { Menu, Layout, Button } from 'antd';
+import { UploadOutlined, UserOutlined, VideoCameraOutlined, LogoutOutlined, HomeOutlined } from '@ant-design/icons';
+const { Header, Content, Footer, Sider } = Layout;
 
-function Admin({ match }) {
-  return <div className="Admin" >
-    我是admin bar
-<Router>
-      <Switch>
-        <Route path={match.url + "/communication"} component></Route>
-        <Route path={match.url + "/home"} component></Route>
-        <Route path={match.url + "/query"} component></Route>
-        <Route path={match.url}>
-          <Redirect to={match.url + '/home'}></Redirect>
-        </Route>
-      </Switch>
-    </Router>
-  </div>
+
+class Admin extends React.Component {
+
+
+  render() {
+    return (
+      <Layout>
+        <Sider
+          className={'noselect'}
+          breakpoint="sm"
+          collapsedWidth="0"
+          onBreakpoint={broken => {
+            console.log(broken);
+          }}
+          onCollapse={(collapsed, type) => {
+            console.log(collapsed, type);
+          }}
+        >
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+            <Menu.Item key="1" icon={<UserOutlined />}>
+              nav 1
+        </Menu.Item>
+            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+              nav 2
+        </Menu.Item>
+            <Menu.Item key="3" icon={<UploadOutlined />}>
+              nav 3
+        </Menu.Item>
+            <Menu.Item key="4" icon={<UserOutlined />}>
+              nav 4
+        </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header className="site-layout-sub-header-background" style={{ padding: 0 }} >
+            <span className={'headerButton'}>
+              <Button className={'home'} size={'small'} icon={<HomeOutlined />}>回首页</Button>
+              <Button danger className={'logout'} size={'small'} icon={<LogoutOutlined />}>登出</Button>
+            </span>
+            <span className={'header noselect'}>
+              管理后台 - 
+            </span>
+          </Header>
+          <Content style={{ margin: '16px 16px 0' }}>
+            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+              content
+        </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        </Layout>
+      </Layout >
+
+
+
+    );
+  }
 }
 
 export default Admin;
+        // <Router>
+        //   <Switch>
+        //     <Route path={"/admin/communication"} component></Route>
+        //     <Route path={"/admin/home"} component></Route>
+        //     <Route path={"/admin/query"} component></Route>
+        //     <Route path={'/admin'}>/admin
+        //   <Redirect to={'/admin/home'}></Redirect>
+        //     </Route>
+        //   </Switch>
+        // </Router> 
--- src/login.jsx
+++ src/login.jsx
@@ -1,10 +1,51 @@
 import React from 'react';
 import './login.scss'
+import { Form, Input, Button, Checkbox, Card } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+
+
+
+const onFinish = values => {
+  console.log('Received values of form: ', values);
+};
 
 function Login() {
-  return <div className = "Login" >
-我是login
-    </div>
+  return <div className="Login noselect" >
+    <Card className={'bread'}>
+      <span className={'title'}>后台登入</span>
+
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{ remember: true }}
+        onFinish={onFinish}
+      >
+        <Form.Item
+          name="username"
+          rules={[{ required: true, message: '请输入用户名' }]}
+        >
+          <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="用户名" />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[{ required: true, message: '请输入密码' }]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="密码"
+          />
+        </Form.Item>
+
+        <Form.Item>
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            登入
+        </Button>
+        </Form.Item>
+      </Form>
+    </Card>
+  </div>
 }
+
 
 export default Login;
